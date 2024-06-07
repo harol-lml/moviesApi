59a305e4657ec3ecd0ce3fe88c95e5c16b2a26e1
harol-lml <hapr0023@gmail.com>
2024-06-07 16:42:18 -0500

    fix data

--- config/database.py
+++ config/database.py
@@ -8,6 +8,6 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 
 database_url = f"sqlite:///{os.path.join(base_dir, sqlite_file_name)}"
 
-engine = create_engine(database_url, echo = true)
+engine = create_engine(database_url, echo=True)
 Session = sessionmaker(bind = engine)
 Base = declarative_base()
